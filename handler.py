4b081f46e98c37faf127ecfbbbeecf623e1b610f
loshan <loshansp2001@gmail.com>
2026-02-20 16:18:07 +0530

    set min_concurrency 1

--- handler.py
+++ handler.py
@@ -178,7 +178,7 @@ class SkinFixOutput(BaseModel):
 class SkinFixApp(
     fal.App,
     keep_alive=100,
-    min_concurrency=0,
+    min_concurrency=1,
     max_concurrency=5,
     name="skin-fix-app2",
 ):
